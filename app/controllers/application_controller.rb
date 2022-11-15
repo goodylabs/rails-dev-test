16a3d42e30669990560e8975188620b4593f2f4c
Weronika <weronika.piotrowska@tdcm.io>
2022-11-15 03:37:36 +0100

    skipp unrecignised callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   check_authorization unless: :devise_controller?
 
   class ActiveAdmin::BaseController
-    skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!, raise: false
     skip_authorization_check
   end
 
