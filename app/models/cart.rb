493cdd5a3239059b5b883f0f99ec6ed8f605a2a5
Weronika <weronika.piotrowska@tdcm.io>
2022-11-15 23:49:24 +0100

    add total price display info to checkout interface

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,4 +5,8 @@ class Cart < ApplicationRecord
   def total_price
     product_orders.map { |po| po.quantity * po.product.price }.sum
   end
+
+  def total_products_number
+    product_orders.pluck(:quantity).sum
+  end
 end
