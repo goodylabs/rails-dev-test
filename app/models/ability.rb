68680c37186c7bfb8a31bf3a3cb0227824a51754
Weronika <weronika.piotrowska@tdcm.io>
2022-11-15 23:09:49 +0100

    adjust ability model to handle all functionalities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,15 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can :manage, :all
+    can [:index], Product
+    can [:show], Cart, id: user.cart&.id
+
+    product_order_ids = ProductOrder.where(cart_id: user.cart&.id).pluck(:id)
+    can [:create], ProductOrder
+    can [:destroy], ProductOrder, id: product_order_ids
+
+    order_ids = Order.where(user_id: user.id).pluck(:id)
+    can [:create], Order
+    can [:index], Order, id: order_ids
   end
 end
