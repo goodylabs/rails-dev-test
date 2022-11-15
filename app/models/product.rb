a7bb8b6c63198d768eb145fefbd66e564233da17
Weronika <weronika.piotrowska@tdcm.io>
2022-11-15 01:41:05 +0100

    add category enum for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ApplicationRecord
   has_many :product_orders
+
+  enum category: {
+    arabica: 0,
+    robusta: 1,
+    mix: 2
+  }
+  CATEGORIES = categories.to_h { |k, _v| [k.to_sym, k] }.freeze
+
+  def self.get_categories
+    CATEGORIES
+  end
+
+  def self.get_human_categories
+    CATEGORIES.to_h { |k, _v| [Cv.human_enum_name(:categories, k), k] }
+  end
 end
