6635b3620626d02fcfa738f9de96eebbf1fa2630
Weronika <weronika.piotrowska@tdcm.io>
2022-11-15 03:32:40 +0100

    add carts controller and views

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,7 @@
+class CartsController < ApplicationController
+  load_and_authorize_resource
+
+  def show
+    # byebug
+  end
+end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :product_orders
+
+  def total_price
+    product_orders.map {|po| po.quantity * po.product.price }.sum
+  end
 end
