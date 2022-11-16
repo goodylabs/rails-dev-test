13b09d2c2cc185222a02fdbeacc0313ca3204526
Weronika <weronika.piotrowska@tdcm.io>
2022-11-16 03:42:57 +0100

    refactorize the code

--- app/controllers/product_orders_controller.rb
+++ app/controllers/product_orders_controller.rb
@@ -2,11 +2,11 @@ class ProductOrdersController < ApplicationController
   load_and_authorize_resource
 
   def create
-    @products = Product.all
     product_orders_svc = ProductOrdersService.new(product_order_params)
     if product_orders_svc.create_or_update
       redirect_to products_path, notice: t('.add_to_cart')
     else
+      @products = Product.all
       @product_order = product_orders_svc.product_order
       respond_to do |format|
         format.js { render 'products/product_tile.js.erb', layout: false }
