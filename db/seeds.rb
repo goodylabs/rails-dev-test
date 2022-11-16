79ae9b1f59acf7087486f212a2baff74e7aebca3
Weronika <weronika.piotrowska@tdcm.io>
2022-11-16 03:58:16 +0100

    adjust password for AdminUser

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Admin
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'Password!', password_confirmation: 'Password!') if Rails.env.development?
 
 # user
 FactoryBot.create(
