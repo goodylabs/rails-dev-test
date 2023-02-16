3ca848bb5d0527b278c3f05ff1b85a309eb3b08d
Daniel Florczak <danyflorczak@gmail.com>
2023-02-16 15:41:19 +0100

    adding basic model tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:line_items).dependent(:destroy) }
+  it { should have_many(:products).through(:line_items) }
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:product) }
+  it { should belong_to(:cart) }
+  it { should belong_to(:order).optional }
 end
