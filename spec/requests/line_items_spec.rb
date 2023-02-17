cbe250823f476cedc789bdd7bc8549ad9f9ff7f5
Daniel Florczak <danyflorczak@gmail.com>
2023-02-17 02:22:13 +0100

    add tests for line items controller

--- spec/requests/line_items_spec.rb
+++ spec/requests/line_items_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "LineItems", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:product) { FactoryBot.create(:product) }
+  let(:cart) { FactoryBot.create(:cart) }
+  let!(:line_item) { FactoryBot.create(:line_item, cart: cart, product: product) }
+
+  describe "POST #create" do
+    context "when the product is already in the cart" do
+      it "increments the quantity of the line item" do
+        post line_items_path, params: { product_id: product.id }
+        expect(assigns(:line_item).quantity).to eq(1)
+      end
+    end
+
+    context "when the product is not in the cart" do
+      it "creates a new line item" do
+        expect {
+          post line_items_path, params: { product_id: FactoryBot.create(:product).id }
+        }.to change(LineItem, :count).by(1)
+      end
+    end
+
+    it "redirects to the cart page" do
+      post line_items_path, params: { product_id: product.id }
+      expect(response).to redirect_to(cart_path(assigns(:current_cart)))
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "deletes the line item" do
+      expect {
+        delete line_item_path(line_item)
+      }.to change(LineItem, :count).by(-1)
+    end
+
+    it "redirects to the cart page" do
+      delete line_item_path(line_item)
+      expect(response).to redirect_to(cart_path(assigns(:current_cart)))
+    end
   end
 end
