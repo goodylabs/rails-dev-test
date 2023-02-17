78b33e0da6cf886e037b664ca34c1f02038db6dd
Daniel Florczak <danyflorczak@gmail.com>
2023-02-17 01:28:31 +0100

    changing carts, line_items and orders factories

--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :line_item do
     quantity { 1 }
-    product_id { 1 }
-    cart_id { 1 }
-    order_id { 1 }
+    product
+    cart
+    order { nil }
   end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order do
-    name { "MyString" }
-    email { "MyString" }
-    address { "MyText" }
+    email { "test@example.com" }
+    address { "123 Main St" }
+    credit_card_number { "4111111111111111" }
   end
 end
