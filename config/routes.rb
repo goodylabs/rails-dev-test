26e25da0180c4690c9b7dfce2f5437ce87c1d07f
Daniel Florczak <danyflorczak@gmail.com>
2023-02-19 11:58:56 +0100

    refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root "products#index"
 
-  get 'carts/:id' => "carts#show", as: "cart"
-  delete 'carts/:id' => "carts#destroy"
-
-  post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
-  post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
-  post 'line_items' => "line_items#create"
-  get 'line_items/:id' => "line_items#show", as: "line_item"
-  delete 'line_items/:id' => "line_items#destroy"
+  resources :carts, only: [:show, :destroy]
+  resources :line_items, only: [:create, :show, :destroy] do
+    member do
+      post :add_quantity
+      post :reduce_quantity
+    end
+  end
 
   resources :products
-  resources :orders 
+  resources :orders, only: [:index, :show, :new, :create]
 end
